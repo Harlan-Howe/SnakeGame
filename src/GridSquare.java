dacd43c895299198f1d540c7ad3bd658a3b6bb8f
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-29 16:39:01 -0600

    GridSquare created with simple drawing command.

--- src/GridSquare.java
+++ src/GridSquare.java
@@ -1,4 +1,53 @@
+import java.awt.*;
+
 public class GridSquare
 {
-    
+    private int xPos, yPos;
+    private int state;
+
+    public GridSquare(int x, int y)
+    {
+        xPos = x;
+        yPos = y;
+        state = Constants.CELL_STATE_EMPTY;
+    }
+
+    public void setState(int state)
+    {
+        this.state = state;
+    }
+
+    public void drawSelf(Graphics g)
+    {
+        g.setColor(Color.BLACK);
+        g.fillRect(xPos, yPos, Constants.CELL_SIZE, Constants.CELL_SIZE);
+        g.setColor(Color.LIGHT_GRAY);
+        g.drawRect(xPos, yPos, Constants.CELL_SIZE, Constants.CELL_SIZE);
+        switch (state)
+        {
+            case Constants.CELL_STATE_WALL -> drawWall(g);
+            case Constants.CELL_STATE_SNAKE_BODY -> drawBody(g);
+            case Constants.CELL_STATE_APPLE -> drawApple(g);
+        }
+    }
+
+    public void drawWall(Graphics g)
+    {
+        g.setColor(Color.YELLOW);
+        g.fillRect(xPos+2, yPos+2, Constants.CELL_SIZE-4, Constants.CELL_SIZE-4);
+    }
+
+    public void drawBody(Graphics g)
+    {
+        g.setColor(Color.GREEN);
+        g.fillOval(xPos+4, yPos+4, Constants.CELL_SIZE-8, Constants.CELL_SIZE-8);
+    }
+
+    public void drawApple(Graphics g)
+    {
+        g.setColor(Color.RED);
+        g.fillOval(xPos+6, yPos+6, Constants.CELL_SIZE-12, Constants.CELL_SIZE-12);
+    }
+
+
 }
