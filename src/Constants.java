d76d40be6dab068877571348700d75ad0bb91db3
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-29 16:32:00 -0600

    Added CELL_STATE constants.

--- src/Constants.java
+++ src/Constants.java
@@ -4,4 +4,9 @@ public interface Constants
     public final int NUM_COLUMNS = 40;
     public final int NUM_ROWS = 40;
 
+    public final int CELL_STATE_EMPTY = 0;
+    public final int CELL_STATE_WALL = 1;
+    public final int CELL_STATE_SNAKE_BODY = 2;
+    public final int CELL_STATE_APPLE = 3;
+
 }
