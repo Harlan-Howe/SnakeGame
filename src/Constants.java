4d3c1c3413e6bcb0c090ec4f763f8ffb88a4901e
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-29 18:04:33 -0600

    added constants for keyboard WASD with option for arrows.

--- src/Constants.java
+++ src/Constants.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+
 public interface Constants
 {
     public final int CELL_SIZE = 15;
@@ -13,5 +15,19 @@ public interface Constants
     public final int DIRECTION_DOWN = 1;
     public final int DIRECTION_LEFT = 2;
     public final int DIRECTION_UP = 3;
+
+    //   Initially set up for WASD controls.
+    public final int leftKey = KeyEvent.VK_A;
+    public final int rightKey = KeyEvent.VK_D;
+    public final int upKey = KeyEvent.VK_W;
+    public final int downKey = KeyEvent.VK_S;
+
+    /* Activate this section instead, if you'd prefer arrow key controls.
+    public final int leftKey = KeyEvent.VK_LEFT;
+    public final int rightKey = KeyEvent.VK_RIGHT;
+    public final int upKey = KeyEvent.VK_UP;
+    public final int downKey = KeyEvent.VK_DOWN;
+    */
+
 
 }
