e6a518cfc5a88dd1ec1dabb74617ab8244fa9d99
Harlan Howe <harlan.howe@kinkaid.org>
2024-12-29 22:44:09 -0600

    Updated cell state list to incorporate body/head/tail and directions

--- src/Constants.java
+++ src/Constants.java
@@ -8,8 +8,16 @@ public interface Constants
 
     public final int CELL_STATE_EMPTY = 0;
     public final int CELL_STATE_WALL = 1;
-    public final int CELL_STATE_SNAKE_BODY = 2;
-    public final int CELL_STATE_APPLE = 3;
+    public final int CELL_STATE_APPLE = 2;
+    public final int CELL_STATE_SNAKE_BODY_E_W = 3;
+    public final int CELL_STATE_SNAKE_BODY_N_S = 4;
+    public final int CELL_STATE_SNAKE_BODY_NE_SW = 5;
+    public final int CELL_STATE_SNAKE_BODY_NW_SE = 6;
+    public final int CELL_STATE_SNAKE_HEAD_W = 7;
+    public final int CELL_STATE_SNAKE_HEAD_S = 8;
+    public final int CELL_STATE_SNAKE_HEAD_E = 9;
+    public final int CELL_STATE_SNAKE_HEAD_N = 10;
+    public final int CELL_STATE_SNAKE_TAIL = 11;
 
     public final int DIRECTION_RIGHT = 0;
     public final int DIRECTION_DOWN = 1;
