0e0f94494f281a8914ba58836464dce3fa7e4d17
Harlan Howe <harlan.howe@kinkaid.org>
2025-01-05 08:03:34 -0600

    Added score panel with a score label and a method to update score.

--- src/SnakeFrame.java
+++ src/SnakeFrame.java
@@ -7,15 +7,17 @@ public class SnakeFrame extends JFrame implements ActionListener
 {
     private JButton resetButton;
     private final MainSnakePanel mainPanel;
+    private JLabel scoreLabel;
 
     public SnakeFrame()
     {
         super("Snake!");
-        setSize(Constants.CELL_SIZE*Constants.NUM_COLUMNS,Constants.CELL_SIZE*Constants.NUM_ROWS+68);
+        setSize(Constants.CELL_SIZE*Constants.NUM_COLUMNS,Constants.CELL_SIZE*Constants.NUM_ROWS+95);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(buildControlPanel(), BorderLayout.NORTH);
+        getContentPane().add(buildScorePanel(), BorderLayout.SOUTH);
         mainPanel = new MainSnakePanel();
         mainPanel.requestFocusInWindow();
         getContentPane().add(mainPanel, BorderLayout.CENTER);
@@ -33,6 +35,18 @@ public class SnakeFrame extends JFrame implements ActionListener
         return result;
     }
 
+    public JPanel buildScorePanel()
+    {
+        JPanel result = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        scoreLabel = new JLabel("Score: 0");
+        result.add(scoreLabel);
+        return result;
+    }
+
+    public void setScore(int score)
+    {
+        scoreLabel.setText(STR."Score: \{score}");
+    }
     @Override
     public void actionPerformed(ActionEvent e)
     {
